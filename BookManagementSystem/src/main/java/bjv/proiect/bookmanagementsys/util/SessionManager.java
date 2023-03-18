c4bde8d9a3223998dd394213a6ba5015d42754be
Alin Bojenovici <mihaialin193@yahoo.com>
2023-03-18 15:07:17 +0200

    BMS - 1 1. Hibernate Config

---
+++ BookManagementSystem/src/main/java/bjv/proiect/bookmanagementsys/util/SessionManager.java
@@ -0,0 +1,30 @@
+package bjv.proiect.bookmanagementsys.util;
+
+import com.sda.practicalproject.model.Consult;
+import com.sda.practicalproject.model.Pet;
+import com.sda.practicalproject.model.Vet;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionManager extends AbstractSessionManager {
+    private static final SessionManager INSTANCE = new SessionManager();
+
+    private SessionManager() {
+
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return INSTANCE.getSessionFactory("pet_clinic");
+    }
+
+    public static void shutdown() {
+        INSTANCE.shutdownSessionManager();
+    }
+
+    @Override
+    protected void setAnnotatedClasses(Configuration configuration) {
+        configuration.addAnnotatedClass(Vet.class);
+        configuration.addAnnotatedClass(Pet.class);
+        configuration.addAnnotatedClass(Consult.class);
+    }
+}
