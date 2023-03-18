667e79f1216fb8f3aa64ed8b04cc578d0414c132
Alin Bojenovici <mihaialin193@yahoo.com>
2023-03-18 15:12:49 +0200

    Test commit

--- BookManagementSystem/src/main/java/bjv/proiect/bookmanagementsys/Main.java
+++ BookManagementSystem/src/main/java/bjv/proiect/bookmanagementsys/Main.java
@@ -1,7 +1,11 @@
 package bjv.proiect.bookmanagementsys;
 
+import bjv.proiect.bookmanagementsys.util.SessionManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SessionManager.getSessionFactory().openSession();
+        System.out.println("asdas");
+        SessionManager.shutdown();
     }
 }
